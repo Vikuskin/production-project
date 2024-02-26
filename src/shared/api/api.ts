fcaa3dac59e40b1f4e1f4e69bc0fcab4ce536983
Kiskina Viktoria <kiskina.vss@yandex.ru>
2024-02-26 19:08:35 +0300

    Fix API instance

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 import { LOCAL_STORAGE_KEYS } from 'shared/constants/constants';
 
 export const API = axios.create({
   baseURL: API_URL,
-  headers: {
-    authorization: localStorage.getItem(LOCAL_STORAGE_KEYS.Auth),
-  },
+});
+
+API.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  config.headers.Authorization = localStorage.getItem(LOCAL_STORAGE_KEYS.Auth) || '';
+
+  return config;
 });
