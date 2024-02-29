6fb111af6cfb25fd96f341347af490e37bee44ae
Kiskina Viktoria <kiskina.vss@yandex.ru>
2024-02-29 12:21:34 +0300

    Fix semantic tags

---
+++ src/shared/ui/Text/types/HeaderTag.ts
@@ -0,0 +1,1 @@
+export type HeaderTagType = 'h1' | 'h2' | 'h3' | 'h4';
--- src/shared/ui/Text/ui/Text.tsx
+++ src/shared/ui/Text/ui/Text.tsx
@@ -4,6 +4,7 @@ import { getClassNames } from 'shared/lib/classNames/getClassNames';
 
 import * as styles from './Text.module.scss';
 
+import { HeaderTagType } from '../types/HeaderTag';
 import { TextAlign } from '../types/TextAlign';
 import { TextSize } from '../types/TextSize';
 import { TextVariant } from '../types/TextVariant';
@@ -17,17 +18,20 @@ interface TextProps {
   size?: TextSize;
 }
 
+const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
+  [TextSize.SizeS]: 'h4',
+  [TextSize.SizeM]: 'h3',
+  [TextSize.SizeL]: 'h2',
+  [TextSize.SizeXl]: 'h1',
+};
+
 export const Text: FC<TextProps> = memo((props: TextProps) => {
   const { text, className, title, variant = TextVariant.Normal, align = TextAlign.Left, size = TextSize.SizeM } = props;
+  const HeaderTag = mapSizeToHeaderTag[size];
 
   return (
     <div data-testid="text-wrapper" className={getClassNames('', [styles[variant], styles[align], className ?? ''])}>
-      {title && (
-        <>
-          <span className={getClassNames(styles.title, [styles[size]])}>{title}</span>
-          <br />
-        </>
-      )}
+      {title && <HeaderTag className={getClassNames(styles.title, [styles[size]])}>{title}</HeaderTag>}
       <span className={getClassNames(styles.text, [styles[size]])}>{text}</span>
     </div>
   );
